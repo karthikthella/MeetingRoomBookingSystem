59162a30812d568c1e63164b55cbc8d0ea672405
Karthik Thella <thella_karthikkumar@epam.com>
2026-02-27 10:35:01 +0530

    Changes

--- frontend/src/main/java/com/epam/meetingroom/Main.java
+++ frontend/src/main/java/com/epam/meetingroom/Main.java
@@ -1,4 +1,4 @@
-package com.epam.pulse.edgefabric;
+package com.epam.meetingroom;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
